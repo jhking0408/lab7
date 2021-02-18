27cdf6274ec13ccf698445e936250f636f4be5c0
jhking0408 <jhking@ucsd.edu>
2021-02-18 10:56:30 -0800

    lab7

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,15 @@ var projects = require('../projects.json');
 exports.view = function(request, response){
   	response.render('index', projects);
 };
+
+data = "http://localhost:3000:/viewAlt"
+
+exports.view = function(request, response){
+	project['viewAlt'] = false;
+	response.render('index', data);
+};
+
+exports.viewAlt = function(request, response){
+	project['viewAlt'] = true;
+	response.render('index', data);
+};
